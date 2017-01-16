b2baab3102fce11492a7f736dcb9e2e05f72d17d
Semenyuk Vladimir <v.semenyuk@corp.mail.ru>
2017-01-17 00:40:04 +0300

    hl-11: model optimizations

--- src/model/Model.ts
+++ src/model/Model.ts
@@ -3,19 +3,22 @@ import { Signal } from '../signal/Signal';
 import { ISignalLike } from '../signal/interface.d';
 import { TModelChangeParam } from './interface.d';
 
-//@cidPrefix('m')
 export class Model<S> extends Receiver {
     public idAttribute: keyof S | 'id' = 'id';
 
+    //public schema: S;
     public readonly schema: {
-        readonly [T in keyof S]: T;
+        readonly [T in keyof S]: S[T];
     } = <any>{};
 
     public readonly signals: {
         readonly change: {
-            readonly [T in keyof S]: ISignalLike<TModelChangeParam<S[T], S>>;
+            [T in keyof S]: Signal<TModelChangeParam<S[T], S>>;
         },
-        readonly changeAny: ISignalLike<TModelChangeParam<Partial<S>, S>>;
+        readonly changeAny: Signal<TModelChangeParam<Partial<S>, S>>;
+    } = {
+        change: <any>{},
+        changeAny: new Signal()
     };
 
     
@@ -42,43 +45,9 @@ export class Model<S> extends Receiver {
     }
 
     private _init(): void {
-        let changeSignalsGettersHash = {};
-
         for (let key of Object.keys(this.schema)) {
-            Object.defineProperty(changeSignalsGettersHash, key, {
-                get(): Signal<any> {
-                    let signal = new Signal();
-
-                    Object.defineProperty(changeSignalsGettersHash, key, {
-                        value: signal
-                    });
-
-                    return signal;
-                }
-            });
+            (<any>this).change[key] = new Signal();
         }
-
-        let signalsHash = {
-            change: changeSignalsGettersHash
-        };
-
-        Object.defineProperties(signalsHash, {
-            changeAny: {
-                get(): Signal<any> {
-                    let signal = new Signal();
-
-                    Object.defineProperty(signalsHash, 'changeAny', {
-                        value: signal
-                    });
-
-                    return signal;
-                }   
-            }
-        });
-
-        Object.defineProperty(this, 'signals', {
-            value: signalsHash
-        });
     }
 
     
